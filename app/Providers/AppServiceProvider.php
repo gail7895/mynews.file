fab02600626deedd6203577269bc8951dfcf42fa
EC2 Default User <ec2-user@ip-172-31-17-183.ec2.internal>
2020-09-15 13:03:44 +0000

    text

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //以下を追記
+        \URL::forceScheme('https');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,14 @@ Route::get('XXX',
 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin'],function() {
-    Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
