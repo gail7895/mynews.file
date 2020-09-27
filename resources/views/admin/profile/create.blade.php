c975889c569aa87d2825d5888b909c64080828d6
EC2 Default User <ec2-user@ip-172-31-17-183.ec2.internal>
2020-09-27 06:56:25 +0000

    profileのviewの追加

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,57 @@
+
+
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'プロフィール作成'を読み込む --}}
+@section('title','プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール</h2>
+              <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach ($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <select class="form-control" name="gender">
+                                <option value="男性">男性</option>
+                                <option value="女性">女性</option>
+                            </select>
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="3">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
